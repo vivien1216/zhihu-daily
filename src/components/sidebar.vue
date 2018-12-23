aa777a5c704cd7ab9e48a41a39ba4c153212391c
vivien <[1870495688@qq.com](mailto:1870495688@qq.com)>
2018-12-23 21:11:33 +0800

    侧边栏静态页面及其简单动画完成

---
+++ src/components/sidebar.vue
@@ -0,0 +1,44 @@
+<template>
+  <transition name="move">
+    <div class="wrap">
+      <div class="sidebar">
+	      <div class="top">
+	        <div class="info">
+	          <img class="avatar" src="../assets/images/avatar.jpg" />
+	          <span class="name">vivien</span>
+	        </div>
+	        <div class="menu-wrapper">
+	          <div class="collect">
+	            <i class="iconfont">&#xe605;</i>
+	            <span>收藏</span>
+	          </div>
+	          <div class="download">
+	            <i class="iconfont">&#xe694;</i>
+	            <span>离线下载</span>
+	          </div>
+	        </div>
+	      </div>
+	      <div class="home" @click="hideSidebar">
+	        <i class="iconfont">&#xe604;</i>
+	        <span>首页</span>
+	      </div>
+	    </div>
+	    <div class="mask" @click="hideSidebar"></div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  methods: {
+    hideSidebar () {
+      this.$emit('hideSidebar');
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/sass/components/sidebar.sass'
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,29 +1,63 @@
 <template>
   <div class="index">
-    <header>
-      <div class="header-left">
-        <i class="iconfont icon-toggle">&#xe61d;</i>
-        <span class="page-home">首页</span>
-      </div>
-      <div class="header-right">
-        <i class="iconfont icon-news">&#xe667;</i>
-        <i class="iconfont icon-tab">&#xe6b9;</i>
-      </div>
-    </header>
-    <swiper></swiper>
-    <news-list></news-list>
+    <mt-loadmore :top-method="loadTop" ref="loadmore">
+	    <header>
+	      <div class="header-left">
+	        <i class="iconfont icon-toggle" @click="toggleSidebar">&#xe61d;</i>
+	        <span class="page-home">首页</span>
+	      </div>
+	      <div class="header-right">
+	        <i class="iconfont icon-news">&#xe667;</i>
+	        <i class="iconfont icon-tab">&#xe6b9;</i>
+	      </div>
+	    </header>
+	    <swiper></swiper>
+	    <news-list></news-list>
+	</mt-loadmore>
+    <sidebar v-show="showSideBar" v-on:hideSidebar="toggleSidebar"></sidebar>
   </div>
 </template>
 
 <script>
 import Swiper from "../components/swiper"
 import newsList from '../components/newsList'
+import sidebar from '../components/sidebar'
+import 'scrolling-element'; //  polyfill 'document.scrollingElement'
 
 export default {
   name: 'Index',
   components: {
     Swiper,
-    newsList
+    newsList,
+    sidebar
+  },
+  data () {
+    return {
+      showSideBar: false
+    }
+  },
+  created() {
+    this.showSideBar = false;
+  },
+  methods: {
+    //切换侧边栏显示状态
+    toggleSidebar () {
+      //侧边栏显示时底部列表不能滚动
+      let scrollTop;
+      if(!this.showSidebar) {  //禁止滑动
+        scrollTop = document.scrollingElement.scrollTop;
+        document.body.style.position = 'fixed';
+        document.body.style.top = -scrollTop + 'px';
+      }else {  //取消禁止滑动
+        document.body.style.position = '';
+        document.scrollingElement.scrollTop = scrollTop;
+      }
+      this.showSideBar = !this.showSideBar;
+    },
+    //下拉刷新数据
+    loadTop() {
+	  this.$refs.loadmore.onTopLoaded();
+    }
   }
 }
 </script>
